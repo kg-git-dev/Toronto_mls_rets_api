6128432d069ddc8434fa0c05adf4e4166b4bf423
Kushal Ghimire <kushalghimire@Kushals-MacBook-Air-2.local>
2023-11-24 02:09:54 +0545

    parser pushed

---
+++ XmlParser/index.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const sax = require('sax');
+
+const initializeXmlObject = require('./xmlConfig');
+
+const xmlPath = './Data/official_data.xml';
+
+// Creating a sax parser
+const parser = sax.createStream(true, { trim: true, normalize: true, lowercase: true });
+
+// Setting up variables to keep track of the current element
+let currentElement = '';
+let insideResidentialProperty = false;
+let insideListing = false;
+
+// Setting up data structures
+let residentialProperties = [];
+let currentProperty = {};
+let startTime;
+let endTime;
+let counter = 0;
+
+// Setting up event handlers
+parser.on('opentag', (node) => {
+    // Update the current element
+    currentElement = node.name;
+
+    // Check if we are inside a ResidentialProperty or Listing
+    if (currentElement === 'ResidentialProperty') {
+        startTime = new Date().getTime();
+        insideResidentialProperty = true;
+        currentProperty = { ...initializeXmlObject }; // Creating a new object instance
+    } else if (insideResidentialProperty && currentElement === 'Listing') {
+        insideListing = true;
+    }
+});
+
+parser.on('text', (text) => {
+    // Testing for null values
+    if (text === 'null') text = null;
+
+    // Testing for boolean
+    if (text === 'Y') { text = true } else if (text === 'N') { text = false };
+
+    // Accumulate the text content if inside a Listing
+    if (insideListing) {
+        currentProperty[currentElement] = text;
+    }
+});
+
+parser.on('closetag', (nodeName) => {
+    // Add the current property to the array when leaving ResidentialProperty
+    if (nodeName === 'ResidentialProperty') {
+        insideResidentialProperty = false;
+        residentialProperties.push(currentProperty);
+        counter++
+    } else if (insideResidentialProperty && nodeName === 'Listing') {
+        insideListing = false;
+    }
+});
+
+parser.on('end', () => {
+    // Log the structured data
+    endTime = new Date().getTime();
+    console.log(`${counter} properties in ${(endTime - startTime)} seconds`);
+
+    const jsonOutput = JSON.stringify(residentialProperties, null, 2); // The '2' argument specifies the number of spaces to use for indentation
+    fs.writeFileSync('./Data/output.json', jsonOutput, 'utf-8');
+    console.log('Data saved to output.json');
+});
+
+// Pipe the XML file into the sax parser
+fs.createReadStream(xmlPath)
+    .pipe(parser)
+    .on('error', (err) => {
+        console.error('Error parsing XML:', err);
+    });
